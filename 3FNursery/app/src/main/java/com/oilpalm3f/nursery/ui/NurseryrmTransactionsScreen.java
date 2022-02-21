1bdd05e31e53b960a53bb1703d1c17f90073ae0d
Roja <roja@calibrage.in>
2022-02-21 15:41:43 +0530

    Rm transactions list Screen

--- 3FNursery/app/src/main/java/com/oilpalm3f/nursery/ui/NurseryrmTransactionsScreen.java
+++ 3FNursery/app/src/main/java/com/oilpalm3f/nursery/ui/NurseryrmTransactionsScreen.java
@@ -3,7 +3,9 @@ package com.oilpalm3f.nursery.ui;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.oilpalm3f.nursery.R;
@@ -30,5 +32,14 @@ public class NurseryrmTransactionsScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nurseryrm_transactions_screen);
+
+        addBtn = findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent selectionscreen = new Intent(NurseryrmTransactionsScreen.this, RMActivityFields.class);
+                startActivity(selectionscreen);
+            }
+        });
     }
 }
