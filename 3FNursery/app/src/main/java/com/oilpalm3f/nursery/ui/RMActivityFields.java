1571d4d3ca3319827dbb16f4de7b01b0657e6600
Mahesh <mahesh.m@calibrage.in>
2022-02-22 10:16:19 +0530

    Validations Added for Fields.

--- 3FNursery/app/src/main/java/com/oilpalm3f/nursery/ui/RMActivityFields.java
+++ 3FNursery/app/src/main/java/com/oilpalm3f/nursery/ui/RMActivityFields.java
@@ -3,13 +3,19 @@ package com.oilpalm3f.nursery.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.oilpalm3f.nursery.R;
+import com.oilpalm3f.nursery.utils.UiUtils;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,6 +24,10 @@ public class RMActivityFields extends AppCompatActivity {
 
     Spinner typespinner, uomSpinner;
     LinearLayout labourlyt, otherlyt;
+    EditText mandaysmale, mandaysfemale, mandaysmaleoutside, mandaysfemaleoutside;
+    EditText expensetype, quantity;
+    ImageView imageView;
+    Button submitBtn;
 
 
     @Override
@@ -25,11 +35,32 @@ public class RMActivityFields extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rmfields);
 
+        init();
+        setviews();
+    }
+
+    private void init() {
+
         typespinner = findViewById(R.id.typeSpinner);
         uomSpinner = findViewById(R.id.uomspinner);
 
         labourlyt = findViewById(R.id.labourslyt);
         otherlyt = findViewById(R.id.otherslyt);
+
+        mandaysmale = findViewById(R.id.mandaysmale);
+        mandaysfemale = findViewById(R.id.mandaysfemale);
+        mandaysmaleoutside = findViewById(R.id.mandaysmaleoutside);
+        mandaysfemaleoutside = findViewById(R.id.mandaysfemaleoutside);
+
+        expensetype = findViewById(R.id.expensetype);
+        quantity = findViewById(R.id.quantity);
+        imageView = findViewById(R.id.rmimageview);
+
+        submitBtn = findViewById(R.id.rmsubmitBtn);
+
+    }
+
+    private void setviews() {
 
         labourlyt.setVisibility(View.GONE);
         otherlyt.setVisibility(View.GONE);
@@ -49,10 +80,10 @@ public class RMActivityFields extends AppCompatActivity {
                     labourlyt.setVisibility(View.GONE);
                     otherlyt.setVisibility(View.GONE);
 
-                } else if(typespinner.getSelectedItemPosition() == 1) {
+                } else if (typespinner.getSelectedItemPosition() == 1) {
                     labourlyt.setVisibility(View.VISIBLE);
                     otherlyt.setVisibility(View.GONE);
-                }else{
+                } else {
 
                     labourlyt.setVisibility(View.GONE);
                     otherlyt.setVisibility(View.VISIBLE);
@@ -67,11 +98,61 @@ public class RMActivityFields extends AppCompatActivity {
         });
 
 
-
         String[] uomSpinnerArr = getResources().getStringArray(R.array.uom_values);
         List<String> uomSpinnerList = Arrays.asList(uomSpinnerArr);
         ArrayAdapter<String> uomSpinnerAdapter = new ArrayAdapter<>(RMActivityFields.this, android.R.layout.simple_spinner_item, uomSpinnerList);
         uomSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         uomSpinner.setAdapter(uomSpinnerAdapter);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validations()){
+
+                    Toast.makeText(RMActivityFields.this, "Submit Success", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
+    public boolean validations() {
+
+        if (typespinner.getSelectedItemPosition() == 0) {
+            UiUtils.showCustomToastMessage("Please Select Type", RMActivityFields.this, 0);
+            return false;
+        }
+
+        if (typespinner.getSelectedItemPosition() == 1) {
+
+            if (mandaysmale.length() != 0 || mandaysfemale.length() != 0 || mandaysmaleoutside.length() != 0 || mandaysfemaleoutside.length() != 0) {
+                //UiUtils.showCustomToastMessage("Please enter atleast one value", RMActivityFields.this, 0);
+                return true;
+            }else{
+
+                UiUtils.showCustomToastMessage("Please enter atleast one value", RMActivityFields.this, 0);
+                return false;
+            }
+        }
+
+        if (typespinner.getSelectedItemPosition() == 2){
+
+            if (TextUtils.isEmpty(expensetype.getText().toString())){
+                UiUtils.showCustomToastMessage("Please Enter Expense Type", RMActivityFields.this, 0);
+                return false;
+            }
+
+            if (TextUtils.isEmpty(quantity.getText().toString())){
+                UiUtils.showCustomToastMessage("Please Enter Quantity", RMActivityFields.this, 0);
+                return false;
+            }
+
+            if (uomSpinner.getSelectedItemPosition() == 0) {
+                UiUtils.showCustomToastMessage("Please Select UOM Type", RMActivityFields.this, 0);
+                return false;
+            }
+        }
+            return true;
+    }
+
 }
