8ef2913972f4fc5b231dd6bf68aee693db30de52
Mahesh <mahesh.m@calibrage.in>
2022-02-25 12:00:59 +0530

    from date and todate ui added

--- 3FNursery/app/src/main/java/com/oilpalm3f/nursery/ui/NurseryrmTransactionsScreen.java
+++ 3FNursery/app/src/main/java/com/oilpalm3f/nursery/ui/NurseryrmTransactionsScreen.java
@@ -5,14 +5,19 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.oilpalm3f.nursery.R;
+import com.oilpalm3f.nursery.common.CommonConstants;
+import com.oilpalm3f.nursery.common.CommonUtils;
 import com.oilpalm3f.nursery.database.DataAccessHandler;
 import com.oilpalm3f.nursery.dbmodels.LandlevellingFields;
 import com.oilpalm3f.nursery.dbmodels.MutipleData;
@@ -21,8 +26,12 @@ import com.oilpalm3f.nursery.ui.Adapter.MultipleEntriesRecyclerViewAdapter;
 import com.oilpalm3f.nursery.ui.Adapter.NurseryrmActivitiesAdapter;
 import com.oilpalm3f.nursery.ui.Adapter.RMTransactionRecyclerViewAdapter;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class NurseryrmTransactionsScreen extends AppCompatActivity {
     RecyclerView Transactionrcv;
@@ -36,6 +45,15 @@ TextView activity_name;
     String btn_visibility;
     int Feild_id;
   RMTransactionRecyclerViewAdapter rmtransactionRecyclerViewAdapter;
+
+    private EditText fromDateEdt, toDateEdt;
+    private String fromDateStr = "";
+    private String toDateStr = "";
+    private Calendar myCalendar = Calendar.getInstance();
+    private Button searchBtn;
+    private String searchQuery = "";
+    public static String SearchCollectionwithoutPlotQuery = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,9 +83,61 @@ TextView activity_name;
         Transactionrcv = findViewById(R.id.Transactionrcv);
         Transactionrcv.setHasFixedSize(true);
         Transactionrcv.setLayoutManager(new LinearLayoutManager(this));
+
+        fromDateEdt = (EditText) findViewById(R.id.fromDate);
+        toDateEdt = (EditText) findViewById(R.id.toDate);
     }
 
     private void setviews() {
+
+        String currentDate = CommonUtils.getcurrentDateTime(CommonConstants.DATE_FORMAT_DDMMYYYY);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        fromDateEdt.setText(sdf.format(new Date()));
+        toDateEdt.setText(sdf.format(new Date()));
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel(0);
+            }
+        };
+
+        final DatePickerDialog.OnDateSetListener toDate = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel(1);
+            }
+        };
+
+        //To Date on Click Listener
+        toDateEdt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(NurseryrmTransactionsScreen.this, toDate, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH));  //date is dateSetListener as per your code in question
+                datePickerDialog.show();
+            }
+        });
+        //From Date on Click Listener
+        fromDateEdt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(NurseryrmTransactionsScreen.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH));  //date is dateSetListener as per your code in question
+                datePickerDialog.show();
+            }
+        });
+
         nurseryrmTransactions();
 
         addBtn.setOnClickListener(new View.OnClickListener() {
@@ -80,6 +150,23 @@ TextView activity_name;
                 startActivity(selectionscreen);
             }
         });
+    }
+
+    private void updateLabel(int type) {
+        String myFormat = "dd-MM-yyyy";
+        String dateFormatter = "yyyy-MM-dd";
+
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        SimpleDateFormat sdf2 = new SimpleDateFormat(dateFormatter, Locale.US);
+
+        if (type == 0) {
+            fromDateStr = sdf2.format(myCalendar.getTime());
+            fromDateEdt.setText(sdf.format(myCalendar.getTime()));
+        } else {
+            toDateStr = sdf2.format(myCalendar.getTime());
+            toDateEdt.setText(sdf.format(myCalendar.getTime()));
+        }
+
     }
 
     private void nurseryrmTransactions() {
